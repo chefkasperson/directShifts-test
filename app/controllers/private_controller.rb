507ffb443f0641ab8ee6b2295188f4811d25488b
chefkasperson <chefkasperson@gmail.com>
2022-05-21 14:01:29 -0700

    updated email controller to only send emails to emails without accounts

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -2,9 +2,15 @@ class PrivateController < ApplicationController
   def send_email
     @user = User.find(params[:options][:user][:id])
     @email = params[:options][:email]
-    UserMailer.referral_email(@user, @email).deliver_now
-    render json: {
-      message: "Your email has been sent"
-    }
+    if User.where("email = ?", @email).first
+      render json: {
+        message: "Your referral is already a member, no message has been sent."
+      }
+    else
+      UserMailer.referral_email(@user, @email).deliver_now
+      render json: {
+        message: "Your email has been sent"
+      }
+    end
   end
 end
